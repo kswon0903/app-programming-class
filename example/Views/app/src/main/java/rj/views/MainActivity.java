7be1ce218ff568169abe88873025bed95e9cb5e1
Unknown <jrj8819@koreatech.ac.kr>
2018-07-18 17:48:29 +0900

    이미지뷰 예제 추가

--- example/Views/app/src/main/java/rj/views/MainActivity.java
+++ example/Views/app/src/main/java/rj/views/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_left, btn_center, btn_right;
+    ImageView imageView_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
         btn_left = (Button)findViewById(R.id.btn_left);
         btn_center = (Button)findViewById(R.id.btn_center);
         btn_right = (Button)findViewById(R.id.btn_right);
+        imageView_image = (ImageView)findViewById(R.id.imageView_image);
 
         btn_left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("btn_listener", "btn_left pressed");
+                Log.d("btn listener", "btn_left pressed");
                 Toast.makeText(getApplicationContext(),
                         "btn_left pressed",
                         Toast.LENGTH_SHORT).show();
@@ -42,10 +45,22 @@ public class MainActivity extends AppCompatActivity {
         btn_right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("btn_listener", "btn_right pressed");
+                Log.d("btn listener", "btn_right pressed");
                 Toast.makeText(getApplicationContext(),
                         "btn_right pressed",
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imageView_image.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View view) {
+                Log.d("image listener", "image pressed");
+                Toast.makeText(getApplicationContext(),
+                        "image pressed",
+                        Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
     }
