6cf7020f8b0ac32474eb1dd4edce03b116982c1b
Unknown <jrj8819@koreatech.ac.kr>
2018-07-19 11:32:02 +0900

    버튼에 토스트 적용

--- example/View2/app/src/main/java/rj/view2/MainActivity.java
+++ example/View2/app/src/main/java/rj/view2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // 버튼 객체를 저장할 맴버 변수를 만든다.
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         button_english = (Button)findViewById(R.id.button_english);
         button_korean = (Button)findViewById(R.id.button_korean);
 
+        // EngListener의 객체 만들기
+        EngListener engListener = new EngListener();
+
+        button_english.setOnClickListener(engListener);
     }
 
     // english 버튼에게 기능을 부여하기 위한 EngListener를 만드는 클래스
@@ -26,7 +31,16 @@ public class MainActivity extends AppCompatActivity {
         // 버튼을 클릭했을 때 실행되는 onClick() 함수
         @Override
         public void onClick(View view) {
-
+            // 앱에서 메시지를 출력하는 Toast
+            // makeText()함수를 활용하여 메시지를 출력한다.
+            // 1번째 매개변수 : getApplicationContext()
+            //               => 앱의 정보를 가져오는 함수
+            // 2번째 매개변수 : 출력하고 싶은 글자(문자열)
+            // 3번째 매개변수 : 글자를 표시하는 시간
+            //    Toast.LENGTH_LONG 이나 Toast.LENGTH_SHORT 중 하나 선택
+            Toast.makeText(getApplicationContext(),
+                    "english 버튼이 클릭되었습니다.",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
