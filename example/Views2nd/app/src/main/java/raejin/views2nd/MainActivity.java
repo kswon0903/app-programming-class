84e3b0f18d6a4d00ce6644f0f7f60d4d88369919
Unknown <jrj8819@koreatech.ac.kr>
2018-07-20 04:58:40 +0900

    videoview 예제 추가

---
+++ example/Views2nd/app/src/main/java/raejin/views2nd/MainActivity.java
@@ -0,0 +1,76 @@
+package raejin.views2nd;
+
+import android.media.Rating;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.RatingBar;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar_rating1, ratingBar_rating2, ratingBar_rating3;
+    VideoView videoView_video;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar_rating1 = (RatingBar)findViewById(R.id.ratingBar_rating1);
+        ratingBar_rating2 = (RatingBar)findViewById(R.id.ratingBar_rating2);
+        ratingBar_rating3 = (RatingBar)findViewById(R.id.ratingBar_rating3);
+
+        videoView_video = (VideoView)findViewById(R.id.videoView_video);
+
+        ratingBar_rating1.setOnRatingBarChangeListener(new RatingListener());
+        ratingBar_rating2.setOnRatingBarChangeListener(new RatingListener());
+        ratingBar_rating3.setOnRatingBarChangeListener(new RatingListener());
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+
+                "/raw/atoms");
+        videoView_video.setVideoURI(uri);
+        videoView_video.start();
+
+        MediaController mediaController = new MediaController(this);
+        videoView_video.setMediaController(mediaController);
+
+
+
+
+    }
+
+    class RatingListener implements RatingBar.OnRatingBarChangeListener {
+
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+            /**
+             * v : 별점
+             * b : 사용자가 조작하여 별점을 내린 경우 true, 아니면 false
+             */
+
+            switch(((View)ratingBar).getId()) {
+                case R.id.ratingBar_rating1:
+                    Toast.makeText(getApplicationContext(),
+                            "제 점수는요? " + String.valueOf(v),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.ratingBar_rating2:
+                    Toast.makeText(getApplicationContext(),
+                            "제 점수는요? " + String.valueOf(v),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.ratingBar_rating3:
+                    Toast.makeText(getApplicationContext(),
+                            "제 점수는요? " + String.valueOf(v),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+}
