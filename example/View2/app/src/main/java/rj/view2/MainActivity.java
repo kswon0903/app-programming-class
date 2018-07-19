b98d179104d862a0135a288bd9f5ec0821236490
Unknown <jrj8819@koreatech.ac.kr>
2018-07-19 15:36:04 +0900

    계산기 예제 레이아웃 작성

--- example/View2/app/src/main/java/rj/view2/MainActivity.java
+++ example/View2/app/src/main/java/rj/view2/MainActivity.java
@@ -2,22 +2,36 @@ package rj.view2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // 버튼 객체를 저장할 맴버 변수를 만든다.
     Button button_english, button_korean;
 
+    // 에디트 택스트 객체를 저장할 맴버 변수를 만들어 놓는다.
+    EditText editText_name, editText_password, editText_number;
+    TextView textView_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculator);
 
         // ID로 레이아웃에 있는 버튼의 객체를 읽어와서 맴버 변수에 저장한다.
         button_english = (Button)findViewById(R.id.button_english);
         button_korean = (Button)findViewById(R.id.button_korean);
+
+        // ID로 레이아웃에 있는 에디트 택스트의 객체를 읽어와서 맴버 변수에 저장한다.
+        editText_name = (EditText)findViewById(R.id.editText_name);
+        editText_number = (EditText)findViewById(R.id.editText_number);
+        editText_password = (EditText)findViewById(R.id.editText_password);
+        textView_text = (TextView)findViewById(R.id.textView_text);
 
         // EngListener의 객체 만들기
         EngListener engListener = new EngListener();
@@ -34,6 +48,53 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+        // TextListener의 객체를 생성한다.
+        TextListener textListener = new TextListener();
+
+        // 생성한 TextListener의 객체를 edittext에 등록
+        editText_name.addTextChangedListener(textListener);
+    }
+
+    // 에디트 텍스트에 입력할 때 호출되는 TextListener의 클래스
+    class TextListener implements TextWatcher {
+
+        // 입력하기 전
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            /**
+             * charSequence : 수정전 내용 charSequence.toString();
+             * i : 입력될 글자의 위치(0부터 시작)
+             * i1 : 입력된 글자 대신 삭제된 기존 글자 수
+             * i2 : 새로 추가되는 글자수
+             */
+        }
+
+        // 텍스트가 수정될 때
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            /**
+             * charSequence : 입력한 내용 charSequence.toString();
+             * i : 입력 글자의 위치(0부터 시작)
+             * i1 : 입력된 글자 대신 삭제된 기존 글자 수
+             * i2 : 새로 추가되는 글자수
+             */
+
+            // textview가 표시하는 문자열을 수정해주는 setText() 함수
+            textView_text.setText(charSequence);
+            /**
+             * textview가 표시하는 문자열을 가져오는 getText() 함수
+             * String my = textView_text.getText();
+             */
+        }
+
+        // 입력 후에
+        @Override
+        public void afterTextChanged(Editable editable) {
+            /**
+             * editable : 수정 후 글자(문자열)
+             */
+        }
     }
 
     class BtnListener implements View.OnClickListener {
