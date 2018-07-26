486e593c7f0bed7c3e7aead6f256857f27515cb1
Unknown <jrj8819@koreatech.ac.kr>
2018-07-26 22:15:40 +0900

    Activity, intent, custom listview 예제 작성

--- example/ListViewExample/app/src/main/java/raejin/listviewexample/ItemDate.java
+++ example/ListViewExample/app/src/main/java/raejin/listviewexample/ItemDate.java
@@ -3,10 +3,12 @@ package raejin.listviewexample;
 public class ItemDate {
     int image_id;
     String name;
+    String desc;
 
-    ItemDate(int image_id, String name) {
+    ItemDate(int image_id, String name, String desc) {
         this.image_id = image_id;
         this.name = name;
+        this.desc = desc;
     }
 
     public int getImage_id() {
@@ -17,11 +19,19 @@ public class ItemDate {
         return name;
     }
 
+    public String getDesc() {
+        return desc;
+    }
+
     public void setImage_id(int image_id) {
         this.image_id = image_id;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 }
